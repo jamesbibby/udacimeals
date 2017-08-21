a72102be17720f81707d938b54907acf950be07b
James Bibby <bibby@udacity.com>
2017-08-21 14:10:31 -0400

    Added multiple reducers and react-redux bindings

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import reducer from './reducers'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-const store = createStore(reducer)
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 console.log(store.getState())
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+)
 registerServiceWorker()
